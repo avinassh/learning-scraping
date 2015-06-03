3d9e34ecf7dede57fe7e577df7692c16a57a307a
avi <avinashsajjan@gmail.com>
2015-06-03 09:47:08 +0530

    initial import of Django project and app

---
+++ scrapy_coco/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Challenge(models.Model):
+    # ideally following both should be in HTML, converted 
+    # from markdown
+    # or render the html at the run time
+    challenge_text = models.TextField()
+    solution = models.TextField()
+    # data for API
+    challenge_data = models.TextField()
+
+    def __str__(self):
+        return self.challenge_text
