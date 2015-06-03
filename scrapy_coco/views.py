15cd401755e7b5fb0c7455fe462956d7e2e86206
avi <avinashsajjan@gmail.com>
2015-06-03 18:38:10 +0530

    edited the views, now it uses custom handler for each challenge

--- scrapy_coco/views.py
+++ scrapy_coco/views.py
@@ -3,24 +3,24 @@ from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext, loader
 
 from .models import Challenge
-# Create your views here.
+from .challenge_handlers import challenge_handler
+
 
 def index(request):
     challenges_list = Challenge.objects.values_list('id', flat=True)
     return render(request, 'scrapy_coco/index.html', {'challenges_list': challenges_list})
 
+
 def challenge(request, challenge_id):
     challenge = get_object_or_404(Challenge, pk=challenge_id)
     return render(request, 'scrapy_coco/challenge.html', {'challenge': challenge})
+
 
 def solution(request, challenge_id):
     challenge = get_object_or_404(Challenge, pk=challenge_id)
     solution = challenge.solution
     return render(request, 'scrapy_coco/solution.html', {'solution': solution})
 
+
 def api(request, challenge_id):
-    challenge = get_object_or_404(Challenge, pk=challenge_id)
-    challenge_data = challenge.challenge_data
-    # use JSON if its dict
-    response = JsonResponse({'data': challenge_data})
-    return HttpResponse(response)
+    return HttpResponse(challenge_handler(request, challenge_id))
